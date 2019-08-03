8bbf6c23c9f4f2073b1917a950f0f28a72daaeb2
ihavenousername <sunnyr447@gmail.com>
2019-08-03 02:32:06 -0700

    Revert "hopethiswillwork"
    This reverts commit 930fbc6cd34c5d11b506f492c5e19b19f049956e.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,26 +1,12 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField]float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f;
-
-    [SerializeField] AudioClip mainEngine;
-    [SerializeField] AudioClip Success;
-    [SerializeField] AudioClip Death;
-
-    [SerializeField] ParticleSystem mainEngineParticles;
-    [SerializeField] ParticleSystem SuccessParticles;
-    [SerializeField] ParticleSystem DeathParticles;
-
-
     AudioSource audioSource;
     Rigidbody rigidBody;
-
-    enum State { Alive, Dying, Transcending }
-    State state = State.Alive;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -31,102 +17,30 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (state == State.Alive)
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // 
         {
-            RespondToThrustInput();
-            RespondToRotateInput();
-        }
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (state != State.Alive)
-        {
-            return; // ignore collisions when dead
-        }
-        switch (collision.gameObject.tag)
-        {
-            case "Friendly":
-                break;
-            case "Finish":
-                StartSuccessSequence();
-                break;
-            default:
-                StartDeathSequence();
-                break;
-        }
-    }
-
-    private void StartDeathSequence()
-    {
-        state = State.Dying;
-        audioSource.Stop();
-        audioSource.PlayOneShot(Death);
-        Invoke("LoadNextLevel", 1f);
-    }
-
-    private void StartSuccessSequence()
-    {
-        state = State.Transcending;
-        audioSource.Stop();
-        audioSource.PlayOneShot(Success);
-        Invoke("LoadNextScene", 1f);
-    }
-
-    private void LoadNextLevel()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    private void LoadNextScene()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    private void RespondToThrustInput()
-    {
-        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
-        {
-            ApplyThrust();
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
         else
         {
-            mainEngineParticles.Stop();
             audioSource.Stop();
         }
-    }
-
-    private void ApplyThrust()
-    {
-        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-        if (!audioSource.isPlaying) // so it doesn't layer 
-        {
-            audioSource.PlayOneShot(mainEngine);
-        }
-        mainEngineParticles.Play();
-    }
-
-    private void RespondToRotateInput()
-    {
-        rigidBody.freezeRotation = true; //take manual control of the rotation
-
-        
-        float RotationThisFrame = rcsThrust * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * RotationThisFrame);
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * RotationThisFrame);
+            transform.Rotate(-Vector3.forward);
         }
-        rigidBody.freezeRotation = true; // resume phisics control of rotation
     }
-    
-
-
-
-
-
-
 }
